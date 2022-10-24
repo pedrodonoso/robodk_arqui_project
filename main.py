c770603e8eed912bc8a414b34c9710dbc1206f2f
pedro donoso <pedro.donosoa@sansano.usm.cl>
2022-10-24 16:29:50 -0300

    update

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from robodk.robomath import *  # basic matrix operations
 
 sys.path.append('src/')
 
-from Movements import Movements as m
+# from Movements import Movements as m
+# from Movements import movement_test
+def movement_test(robot):
+    # calculate a new approach position 100 mm along the Z axis of the tool with respect to the target
+    approach = robot.Pose()*transl(0,0,-10)
+    robot.MoveL(approach)               # linear move to the approach position
+    original = robot.Pose()*transl(0,0,10)
+    robot.MoveL(original) 
+    return True
+
 from Connections import Connections as c
 from lib import connect_by_name as cbn
 # import src.lib.connect as connect
@@ -43,5 +52,5 @@ ROBOT_IP = os.environ['ROBOT_IP']
 RDK, robot = cbn.connect_by_name(RDK=RDK, name_robot="UR5", path_station=PATH_STATION, robot_ip=ROBOT_IP)
 
 # print("Conectado!!!!...")
-m.movement_test(robot=robot)
+movement_test(robot=robot)
 # m.moverobotWASD(RDK=RDK, robot=robot)
